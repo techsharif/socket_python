da9d2abbaef9751e423a92f94ba6fea41e7e7422
techsharif <sharif.cse.hstu@gmail.com>
2017-08-02 11:07:36 +0600

    init commit

---
+++ client.py
@@ -0,0 +1,88 @@
+
+import socket
+import threading
+import time
+import hashlib
+
+tLock = threading.Lock()
+shutdown = False
+res = [False,0]
+def receving(name, sock):
+    while not shutdown:
+        try:
+            tLock.acquire()
+            while True:
+                data, addr = sock.recvfrom(1024)
+                global res 
+                res = [True,data]
+        except:
+            pass
+        finally:
+            tLock.release()
+         
+       
+            
+            
+
+host = '127.0.0.1'
+port = 0
+
+server = ('127.0.0.1',5000)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+s.setblocking(0)
+
+rT = threading.Thread(target=receving, args=("RecvThread",s))
+rT.start()
+
+alias = raw_input("Name: ")
+message = ''
+while message=='':
+    message = raw_input(alias + "-> ")
+t = message
+mt = ''
+key = ''
+while key == '':
+    s.sendto(alias + ": " + message, server)
+    print res
+    if res[0]:
+        data = res[1]
+        if len(data.split(':')) == 2:
+            st = int(data.split(':')[0])
+            e = int(data.split(':')[1])
+            for k in range(st,e+1):
+                m = hashlib.md5()
+                m.update(str(t)+str(k))
+                if str(m.hexdigest()) == mt:
+                    print '-----------------------------------'
+                    print 'Key: ' + str(k)
+                    print '-----------------------------------'
+                    key = str(k)
+                    break
+            if key !='': break
+        elif len(data.split(':')) == 4: 
+            d = data.split(':')
+            mt = d[1]
+            st = int(d[2])
+            e = int(d[3])
+            print(mt)
+            for k in range(st,e+1):
+                m = hashlib.md5()
+                m.update(str(t)+str(k))
+                if str(m.hexdigest()) == mt:
+                    print '-----------------------------------'
+                    print 'Key: ' + str(k)
+                    print '-----------------------------------'
+                    key = str(k)
+                    break
+            if key !='': break
+            
+        print 'not found in range - ' + res[1]
+        print 'resend'
+    message = 'resend'
+    time.sleep(0.1)
+
+shudown = True
+rT.join()
+s.close()
---
+++ server.py
@@ -0,0 +1,56 @@
+
+import socket
+import time
+import random
+import hashlib
+host = '127.0.0.1'
+port = 5000
+
+# clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host,port))
+s.setblocking(0)
+
+quitting = False
+print "Server Started."
+while not quitting:
+    try:
+        data, client = s.recvfrom(1024)
+
+        
+        if "Quit" in str(data):
+            quitting = True
+
+
+        tdata = data
+        data = data.split(': ')[1]
+        if data!='resend':
+            print 'request: '+data
+            print time.ctime(time.time()) + str(client) + ": :" + str(tdata)
+
+        
+
+        a = 0
+        b = 9
+        key = random.randint(a, b)
+        range_start = random.randint(a, b)
+        range_end = random.randint(range_start, b)
+        
+        if data=='resend':
+            # print str(range_start)+':'+str(range_end)
+            s.sendto(str(range_start)+':'+str(range_end), client)
+        else: 
+            m = hashlib.md5()
+            m.update(str(data)+str(key))
+            # print(str(m.hexdigest()))
+            t = data
+            mt = str(m.hexdigest())
+            rng = str(range_start)+':'+str(range_end)
+            message = t+':'+mt+':'+rng
+            print message
+            print 'Key: ' + str(key)
+            s.sendto(message, client)
+    except:
+        pass
+s.close()
